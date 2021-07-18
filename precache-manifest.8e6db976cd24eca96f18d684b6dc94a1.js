a5fffd7c37b762662914b1844b5141c722031992
Rahul7549 <67723224+Rahul7549@users.noreply.github.com>
2021-07-18 15:23:56 +0530

    Updates

---
+++ precache-manifest.8e6db976cd24eca96f18d684b6dc94a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1cbc74a843020f14e77535e92cb8b55",
+    "url": "/weather-status/index.html"
+  },
+  {
+    "revision": "4847274c71c38856cc7a",
+    "url": "/weather-status/static/css/main.098ae511.chunk.css"
+  },
+  {
+    "revision": "0d72323e96aecee53d33",
+    "url": "/weather-status/static/js/2.d8bbbe97.chunk.js"
+  },
+  {
+    "revision": "04b21ccc9109def6e71671ed6f2d794a",
+    "url": "/weather-status/static/js/2.d8bbbe97.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4847274c71c38856cc7a",
+    "url": "/weather-status/static/js/main.7131d4cb.chunk.js"
+  },
+  {
+    "revision": "3c8f50e193f2a667305b",
+    "url": "/weather-status/static/js/runtime-main.63e8c834.js"
+  }
+]);
